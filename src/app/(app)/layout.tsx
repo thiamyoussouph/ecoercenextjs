b72f0f867bcf1977b0ddf9836ee8ec2d8e996206
thiamyoussouph <thiamasjo@gmail.com>
2026-01-20 23:31:30 +0000

    fix: chore: deploy

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import type { ReactNode } from 'react'
 
 import { AdminBar } from '@/components/AdminBar'
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -42,6 +42,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || '',
     },
+    push: true,
   }),
   editor: lexicalEditor({
     features: () => {
