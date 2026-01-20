cb56dd1a445b77dbf5d328f186ffdf32f7995d43
thiamyoussouph <thiamasjo@gmail.com>
2026-01-20 22:45:51 +0000

    fix: force dynamic for build

--- src/app/(app)/[slug]/page.tsx
+++ src/app/(app)/[slug]/page.tsx
@@ -11,7 +11,7 @@ import React from 'react'
 
 import type { Page } from '@/payload-types'
 import { notFound } from 'next/navigation'
-
+export const dynamic = 'force-dynamic'
 export async function generateStaticParams() {
   const payload = await getPayload({ config: configPromise })
   const pages = await payload.find({
